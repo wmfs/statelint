0406aa6a96fb1aba477c720f75f9c4f5bd57842b
Jez Higgins <jez@jezuk.co.uk>
2018-10-08 11:41:28 +0100

    feat: Statelint complete.

--- test/statelint_test.js
+++ test/statelint_test.js
@@ -75,10 +75,160 @@ describe('StateMachineLint', () => {
   })
 })
 
+describe('StateMachineLint.StateNode', () => {
+  verify(
+    'should find missing StartAt targets',
+    {
+      StartAt: 'x',
+      States: {
+        y: {
+          Type: 'Succeed'
+        }
+      }
+    },
+    2
+  )
+
+  verify(
+    'should catch nested problems',
+    {
+      StartAt: 'x',
+      States: {
+        x: {
+          StartAt: 'z',
+          States: {
+            w: 1
+          }
+        }
+      }
+    },
+    8
+  )
+
+  verify(
+    'should find States.ALL not in last position',
+    {
+      Retry: [
+        { ErrorEquals: ['States.ALL'] },
+        { ErrorEquals: ['YET ANOTHER'] }
+      ]
+    },
+    6
+  )
+
+  verify(
+    'should find States.ALL not by itself',
+    {
+      Retry: [
+        { ErrorEquals: ['YET ANOTHER'] },
+        { ErrorEquals: ['States.ALL', 'other'] }
+      ]
+    },
+    6
+  )
+
+  verify(
+    'should use Default field correctly',
+    {
+      StartAt: 'A',
+      States: {
+        A: {
+          Type: 'Choice',
+          Choices: [
+            {
+              Variable: '$.a',
+              StringEquals: 'whoo',
+              Next: 'B'
+            }
+          ],
+          Default: 'C'
+        },
+        B: {
+          Type: 'Succeed'
+        },
+        C: {
+          Type: 'Succeed'
+        }
+      }
+    },
+    0
+  )
+
+  verify(
+    'should find Next fields with targets that don\'t match state names',
+    {
+      StartAt: 'A',
+      States: {
+        A: {
+          Type: 'Pass',
+          Next: 'B'
+        }
+      }
+    },
+    2
+  )
+
+  verify(
+    'should find un-pointed-to states',
+    {
+      StartAt: 'A',
+      States: {
+        A: {
+          Type: 'Succeed'
+        },
+        X: {
+          Type: 'Succeed'
+        }
+      }
+    },
+    1
+  )
+
+  verify(
+    'should find missing terminal state',
+    {
+      StartAt: 'A',
+      States: {
+        A: {
+          Type: 'Pass',
+          Next: 'B'
+        },
+        B: {
+          Type: 'Pass',
+          Next: 'A'
+        }
+      }
+    },
+    1
+  )
+
+  verify(
+    'should handle complex missing terminal',
+    require('./fixtures/no-terminal.json'),
+    1
+  )
+
+  verify(
+    'should catch linkage from one parallel branch to another',
+    require('./fixtures/linked-parallel.json'),
+    4
+  )
+
+  verify(
+    'should catch duplicate state names, even in parallels',
+    require('./fixtures/has-dupes.json'),
+    1
+  )
+})
+
+
 function verify (label, json, count, msg) {
   it(label, () => {
     const linter = stateLint()
     const problems = linter.validate(json)
+
+    console.log(problems)
+
     expect(problems.length).to.eql(count)
     if (msg) {
       expect(problems[0]).to.include(msg)
