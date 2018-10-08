9dcee0dc237df12fd655f13e1395709b0157d70d
Jez Higgins <jez@jezuk.co.uk>
2018-10-08 10:32:11 +0100

    test: Remaining stateNode tests

--- test/state_node_test.js
+++ test/state_node_test.js
@@ -7,25 +7,22 @@ const expect = chai.expect
 const stateNode = require('../lib/statelint/state_node')
 
 describe('StateMachineLint::StateNode', () => {
-  it('should find missing StartAt targets', () => {
-    const json = {
+  verify(
+    'should find missing StartAt targets',
+    {
       StartAt: 'x',
       States: {
         y: {
           Type: 'Succeed'
         }
       }
-    }
+    },
+    2
+  )
 
-    const problems = []
-    const checker = stateNode()
-    checker.check(json, 'a.b', problems)
-
-    expect(problems.length).to.eql(2)
-  })
-
-  it('should catch nested problems', () => {
-    const json = {
+  verify(
+    'should catch nested problems',
+    {
       StartAt: 'x',
       States: {
         x: {
@@ -35,156 +32,130 @@ describe('StateMachineLint::StateNode', () => {
           }
         }
       }
-    }
+    },
+    4
+  )
+
+  verify(
+    'should find States.ALL not in }) position',
+    {
+      Retry: [
+        { ErrorEquals: ['States.ALL', 'other'] },
+        { ErrorEquals: ['YET ANOTHER'] }
+      ]
+    },
+    1
+  )
+
+  verify(
+    'should find States.ALL not by itself',
+    {
+      Retry: [
+        { ErrorEquals: ['YET ANOTHER'] },
+        { ErrorEquals: ['States.ALL', 'other'] }
+      ]
+    },
+    1
+  )
+
+  verify(
+    'should use Default field correctly',
+    {
+      StartAt: 'A',
+      States: {
+        A: {
+          Type: 'Choice',
+          Choices: [
+            {
+              Variable: '$.a',
+              Next: 'B'
+            }
+          ],
+          Default: 'C'
+        },
+        B: {
+          Type: 'Succeed'
+        },
+        C: {
+          Type: 'Succeed'
+        }
+      }
+    },
+    0
+  )
+
+  verify(
+    'should find Next fields with targets that don\'t match state names',
+    {
+      StartAt: 'A',
+      States: {
+        A: {
+          Type: 'Pass',
+          Next: 'B'
+        }
+      }
+    },
+    2
+  )
+
+  verify(
+    'should find un-pointed-to states',
+    {
+      StartAt: 'A',
+      States: {
+        A: {
+          Type: 'Succeed'
+        },
+        X: {
+          Type: 'Succeed'
+        }
+      }
+    },
+    1
+  )
+
+  verify(
+    'should find missing terminal state',
+    {
+      StartAt: 'A',
+      States: {
+        A: {
+          Type: 'Pass',
+          Next: 'B',
+        },
+        B: {
+          Type: 'C',
+          Next: 'A'
+        }
+      }
+    },
+    1
+  )
+
+  verify(
+    'should handle complex missing terminal',
+    require('./fixtures/no-terminal.json'),
+    1
+  )
+
+  verify(
+    'should catch linkage from one parallel branch to another',
+    require('./fixtures/linked-parallel.json'),
+    4
+  )
+
+  verify(
+    'should catch duplicate state names, even in parallels',
+    require('./fixtures/has-dupes.json'),
+    1
+  )
+})
+
+function verify(title, json, count) {
+  it(title, () => {
     const problems = []
     const checker = stateNode()
     checker.check(json, 'a.b', problems)
-    expect(problems.length).to.eql(4)
+    expect(problems.length).to.eql(count)
   })
-
-
-  /*
-      it 'should find States.ALL not in end position' do
-        json = '{ "Retry": [' +
-               '  { "ErrorEquals": [ "States.ALL", "other" ] },'  +
-               '  { "ErrorEquals": [ "YET ANOTHER" ] } ' +
-               ' ] ' +
-               '}'
-        json = JSON.parse json
-        problems = []
-        checker = stateNode()
-        checker.check(json, 'a.b', problems)
-        expect(problems.length).to.eql(1)
-      end
-
-      it 'should find States.ALL not by itself' do
-        json = '{ "Retry": [' +
-               '  { "ErrorEquals": [ "YET ANOTHER" ] }, ' +
-               '  { "ErrorEquals": [ "States.ALL", "other" ] }'  +
-               ' ] ' +
-               '}'
-        json = JSON.parse json
-        problems = []
-        checker = stateNode()
-        checker.check(json, 'a.b', problems)
-        expect(problems.length).to.eql(1)
-      end
-
-      it 'should use Default field correctly' do
-        text = {
-          "StartAt"=> "A",
-          "States"=> {
-            "A" => {
-              "Type" => "Choice",
-              "Choices" => [
-                {
-                  "Variable" => "$.a",
-                  "Next" => "B"
-                }
-              ],
-              "Default" => "C"
-            },
-            "B" => {
-              "Type" => "Succeed"
-            },
-            "C" => {
-              "Type" => "Succeed"
-            }
-          }
-        }
-        json = JSON.parse(JSON.pretty_generate(text))
-        problems = []
-        checker = stateNode()
-        checker.check(json, 'a.b', problems)
-        expect(problems.length).to.eql(0)
-      end
-
-      it "should find Next fields with targets that don't match state names" do
-        text = {
-          "StartAt"=> "A",
-          "States"=> {
-            "A" => {
-              "Type" => "Pass",
-              "Next" => "B"
-            }
-          }
-        }
-        json = JSON.parse(JSON.pretty_generate(text))
-        problems = []
-        checker = stateNode()
-        checker.check(json, 'a.b', problems)
-        expect(problems.length).to.eql(2)
-      end
-
-      it "should find un-pointed-to states" do
-        text = {
-          "StartAt"=> "A",
-          "States"=> {
-            "A" => {
-              "Type" => "Succeed"
-            },
-            "X" => {
-              "Type" => "Succeed"
-            }
-          }
-        }
-        json = JSON.parse(JSON.pretty_generate(text))
-        problems = []
-        checker = stateNode()
-        checker.check(json, 'a.b', problems)
-        expect(problems.length).to.eql(1)
-      end
-
-      it "should find missing terminal state" do
-        text = {
-          "StartAt"=> "A",
-          "States"=> {
-            "A" => {
-              "Type" => "Pass",
-              "Next" => "B",
-            },
-            "B" => {
-              "Type" => "C",
-              "Next" => "A"
-            }
-          }
-        }
-        json = JSON.parse(JSON.pretty_generate(text))
-        problems = []
-        checker = stateNode()
-        checker.check(json, 'a.b', problems)
-        expect(problems.length).to.eql(1)
-      end
-
-      it 'should handle complex missing terminal' do
-        j = File.read "test/no-terminal.json"
-        j = JSON.parse j
-        problems = []
-        checker = stateNode()
-        checker.check(j, 'a.b', problems)
-        expect(problems.length).to.eql(1)
-      end
-
-      it 'should catch linkage from one parallel branch to another' do
-        j = File.read "test/linked-parallel.json"
-        j = JSON.parse j
-        problems = []
-        checker = stateNode()
-        checker.check(j, 'a.b', problems)
-        expect(problems.length).to.eql(4)
-      end
-
-      it 'should catch duplicate state names, even in parallels' do
-        j = File.read "test/has-dupes.json"
-        j = JSON.parse j
-        problems = []
-        checker = stateNode()
-        checker.check(j, 'a.b', problems)
-        expect(problems.length).to.eql(1)
-        // problems.each {|p| puts "P #{p}"}
-      end
-
-    end
-
-     */
-})
+} // verify
