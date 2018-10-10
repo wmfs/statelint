5a39129b75c713a1efbdbcc1117b37de6380d9e0
Jez Higgins <jez@jezuk.co.uk>
2018-10-10 10:31:41 +0100

    feat: Update j2119 from 1.3.0 to 1.4.0. Add StateLint class to module exports

--- bin/statelint.js
+++ bin/statelint.js
@@ -1,5 +1,5 @@
 #! /usr/bin/env node
-
+const LineReader = require('n-readlines')
 const stateLint = require('../lib/')()
 
 const args = process.argv.slice(2)
@@ -13,13 +13,37 @@ if (args.length === 1 && ['--help', '-h', '-?'].includes(args[0])) {
 
 let ok = true
 for (const file of args) {
-  const problems = stateLint.validate(file)
+  try {
+    validate(file)
+  } catch (err) {
+    console.log(`ERROR: ${file} os not valid\n\t${err.message}`)
+    ok = false
+  }
+} // for ...
+
+process.exit(ok ? 0 : 1)
+
+/// ////////////////////////
+function validate (file) {
+  const json = readAndParse(file)
+  const problems = stateLint.validate(json)
   if (problems.length) {
     console.log(`ERROR: ${file} is not valid`)
     problems.forEach(p => console.log(`\t${p}`))
 
     ok = false
   }
-}
+} // validate
 
-process.exit(ok ? 0 : 1)
+function readAndParse (jsonSource) {
+  const lines = [...readlines(jsonSource)].join('\n')
+  return JSON.parse(lines)
+} // readAndParse
+
+function * readlines (source) {
+  const reader = new LineReader(source)
+  let line
+  while ((line = reader.next())) {
+    yield line.toString('utf-8')
+  }
+} // readlines
